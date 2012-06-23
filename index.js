c6256d8ec51738410327675565cddcb5a7b25bd8
aleafs <zhangxc83@gmail.com>
2012-06-23 12:31:12 +0800

    Add index.js and test script

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-exports.create = function () {
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+
+var fs   = require('fs');
+var Util = require('util');
+
+var iError = function (name, message) {
+  var _me   = new Error(message);
+  _me.name  = name.toString().trim();
+  return _me;
 };
+
+exports.create = function (flist, prefix, options) {
+
+  var _result   = {};
+
+  /**
+   * @ on complete
+   */
+  var _complete = function (error, filelist) {
+  };
+
+  /**
+   * @ start to split a new file
+   */
+  var start = function (i) {
+    var _fn = flist[i];
+
+    if (!flist[i + 1]) {
+      return _complete(null, _result);
+    }
+
+    start(i + 1);
+  };
+
+  return function (callback) {
+    _complete = callback;
+    if (!Array.isArray(flist) || !flist[0]) {
+      return _complete(iError('EmptyFileList', 'file list is required.'));
+    }
+    start(0);
+  };
+};
+
--- test/filesplit.test.js
+++ test/filesplit.test.js
@@ -1,5 +1,20 @@
+/* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
+
+var should  = require('should');
 var splitor = require(__dirname + '/../');
 
 describe('file split', function () {
 
+  /* {{{ should_callback_error_when_empty_filelist() */
+  it('should_callback_error_when_empty_filelist', function (done) {
+    var split = splitor.create();
+    split(function (error, result) {
+      should.ok(error);
+      error.should.have.property('name', 'EmptyFileList');
+      done();
+    });
+  });
+  /* }}} */
+
 });
+
